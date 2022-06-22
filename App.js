efb0ba692fc2ac613455a4640e449e5cc9801f55
Yusuf Boran <ybsrfn@gmail.com>
2022-06-22 17:43:11 +0300

    button component

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import CarItem from "./component/CarItem/Index";
-
 export default function App() {
   return (
     <View style={styles.container}>
