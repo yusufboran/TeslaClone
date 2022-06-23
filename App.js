16c0e57e88d1f91a46668c43e3ae7f80189c8e8b
Yusuf Boran <ybsrfn@gmail.com>
2022-06-23 18:14:32 +0300

    finaly

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import CarItem from "./component/CarItem/Index";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './component/Header/index';
+import CarsList from './component/CarsList/index'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
-
+      <Header/>
+      <CarsList/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- component/CarItem/Index.js
+++ component/CarItem/Index.js
@@ -1,31 +1,48 @@
-import React from "react";
-import { Text, View, ImageBackground } from "react-native";
-import styles from "./style";
-import StyleButton from "../StyledButton";
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from "../StyledButton";
+import styles from './styles';
 
- 
-const CarItem = () => {
+const CarItem = (props) => {
+
+  const { name, tagline, taglineCTA, image } = props.car;
+
   return (
-    <View style={styles.carContanier}>
+    <View style={styles.carContainer}>
       <ImageBackground
-        source={require("../../assets/images/ModelX.jpeg")}
+        source={image}
         style={styles.image}
       />
+
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline}
+          {' '}
+          <Text style={styles.subtitleCTA}>
+            {taglineCTA}
+          </Text>
+        </Text>
       </View>
-      <StyleButton 
-      type="primary" 
-      content={"Custom Order"}
-      onPress={()=> console.log("Custom Order")
-      } />
-       <StyleButton 
-      type="secondary" 
-      content={"Existing Inventory"}
-      onPress={()=> console.log("Existing Inventory")
-      } />
-     
+
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {
+            console.warn("Custom Order was pressed");
+          }}
+        />
+
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory was pressed");
+          }}
+        />
+      </View>
+
     </View>
   );
 };
